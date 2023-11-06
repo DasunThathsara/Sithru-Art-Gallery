4ee2e83eaeac6da3b9880653834a156294e5b9de
DasunThathsara <dasun.thathsara.sri@gmail.com>
2023-11-06 21:24:03 +0530

    Update navbar

---
+++ includes/navbar.php
@@ -0,0 +1,25 @@
+<nav>
+    <ul>
+        <li><a href="#home">Sith Ru</a></li>
+        <li style="float:right"><a href="#contact-us">Contact</a></li>
+        <li style="float:right"><a href="#contact">Gallery</a></li>
+        <li style="float:right"><a href="#about">About</a></li>
+        <li class="dropdown" style="float:right">
+            <a href="javascript:void(0)" class="dropbtn">Services</a>
+            <div class="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+            </div>
+        </li>
+        <li class="dropdown" style="float:right">
+            <a href="javascript:void(0)" class="dropbtn">Portfolio</a>
+            <div class="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+            </div>
+        </li>
+        <li style="float:right"><a href="#about">Home</a></li>
+    </ul>
+</nav>
--- index.php
+++ index.php
@@ -1,70 +1,59 @@
 <?php
-$titlle = 'EASEL Magazine';
-include_once 'includes/header.php';
-include_once 'includes/dbh.inc.php';
+    $title = 'SithRu';
+    include_once './includes/header.php';
 ?>
-    <div class="container-fluid">
-        <div class="px-lg-5">
 
-            <!-- For demo purpose -->
-            <div class="row d-block"> <!--  mx-auto mt-4  -->
-                <div class="col-lg-12"> <!--  mx-auto -->
-                    <!-- <div class="text-white p-5 shadow-sm rounded">  p-5 banner -->
-                        <!-- <h1 class="display-4">EASEL Magazine</h1>
-                        <p class="lead">Subtitle</p>
-                        <p class="lead">Another subtitle.</p> -->
-                        <img src="banner.jpg" class="img-fluid card-img-top img-thumbnail rounded mt-4" alt="Dispaly banner">
-                    <!-- </div> -->
-                </div>
+<?php
+    include_once './includes/navbar.php';
+?>
+
+<section id="slide-show" class="slide-show">
+    <img src="./images/2.png" alt="">
+</section>
+
+<section id="welcome-details" class="welcome-details">
+    <div class="container">
+        <p>Call Us 0728892</p>
+        <p>WhatsApp 2811918</p>
+    </div>
+</section>
+
+<section id="recent-img" class="recent-img">
+    <h2>Recent Images</h2>
+    <div class="images">
+        <div class="image">
+            <img src="./images/1.jpg" alt="">
+            <div class="content">
+                <h3>Image 1</h3>
+                <p>Size: 1x1 <br>Price: 1</p>
             </div>
-            <!-- End -->
-
-
-
-            <div class="row p-4 justify-content-center">
-
-                <a href="views/user/register.php"><button type="button" class="btn btn-outline-primary mt-2 mb-4 btn-block" data-mdb-ripple-color="info"><h4>ඔබටත් මෙහි Article එකක් පලකර ගැනීමට අවශ්‍ය ද ?</h4></button></a>
-                
-                <h4 calss="pb-2 pt-4">2023</h4>
-                <hr>
-                <?php
-
-                $sql = "SELECT * FROM magazine ORDER BY id DESC";
-                $res = mysqli_query($conn, $sql);
-
-                if (mysqli_num_rows($res) > 0){
-                    while ($row = mysqli_fetch_assoc($res)){ ?>
-
-                <!-- Gallery item -->
-
-                    <div class="col-xl-3 col-lg-4 col-md-5 col-sm-6 col-10 mb-4 d-block cards align-self-center">
-                        <?php echo "<a href='views/user/view.php?id=$row[id]'>" ?>
-                            <div class="bg-white rounded shadow">
-                                <img src="mag_img/<?=$row['photo']?>" alt="" class="img-fluid card-img-top img-thumbnail rounded">
-                                <div class="p-4 inner">
-                                    <h5 class="text-dark"><?=$row['title']?></h5>
-                                    <p class="small text-muted mb-0"><?=$row['description']?></p>
-                                </div>
-                            </div>
-                        </a>
-                        <?php echo "<a href='views/user/view.php?id=$row[id]'>" ?>
-                            <div class="middle">
-                                <div class="textt">View</div>
-                            </div>
-                        </a>
-                    </div>
-
-                    <!-- End -->
-                <?php
-                    }
-                }
-                ?>
+        </div>
+        <div class="image">
+            <img src="./images/2.png" alt="">
+            <div class="content">
+                <h3>Image 1</h3>
+                <p>Size: 1x1 <br>Price: 1</p>
             </div>
-
-            <!-- <button type="button" class="btn btn-outline-info" data-mdb-ripple-color="dark">ඔබටත් මෙහි Article එකක් පලකර ගැනීමට අවශ්‍ය ද ?</button> -->
-
+        </div>
+        <div class="image">
+            <img src="./images/3.jpg" alt="">
+            <div class="content">
+                <h3>Image 1</h3>
+                <p>Size: 1x1 <br>Price: 1</p>
+            </div>
         </div>
     </div>
+</section>
+
+<section id="contact-us" class="contact-us">
+    <div class="container">
+        <div class="left">
+            <h1>S</h1>
+        </div>
+        <div class="right"></div>
+    </div>
+</section>
+
 <?php
-include_once 'includes/footer.php';
+    include_once 'includes/footer.php';
 ?>
